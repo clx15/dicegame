9c4f55918ec88f35dfa74db9d1e3ac19f5ddddb7
clx15 <158769521+clx15@users.noreply.github.com>
2024-03-07 20:44:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
+let random1 = 0;
+let random2 = 0;
+
 function diceRoll(){
     const images1 = ['dice1.png', 'dice2.png', 'dice3.png', 'dice4.png', 'dice5.png', 'dice6.png', ]
-    const random1 = Math.floor(Math.random()*images1.length)
+    random1 = Math.floor(Math.random()*images1.length)
     document.getElementById("img1").src = "images/" + images1[random1];
+    getWinner();
 }
 
 function diceRoll2(){
     const images2 = ['dice1.png', 'dice2.png', 'dice3.png', 'dice4.png', 'dice5.png', 'dice6.png', ]
-    const random2 = Math.floor(Math.random()*images2.length)
+    random2 = Math.floor(Math.random()*images2.length)
     document.getElementById("img2").src = "images/" + images2[random2];
+    getWinner();
 }
 function rollTheDice(){
     diceRoll();
@@ -15,3 +20,13 @@ function rollTheDice(){
 }
 
 document.getElementById("diceRoller").addEventListener("click", rollTheDice);
+
+function getWinner(){
+    if (random1 === random2){
+        document.getElementById("winner").innerText = "Draw";
+    } else if (random1 > random2) {
+        document.getElementById("winner").innerText = "Player 1 won";
+    } else {
+        document.getElementById("winner").innerText = "Player 2 won";
+    }
+}
